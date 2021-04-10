f9ea245ba3485fc2045fd60197a309ead3d5c82f
Linh <cluelesslinh@gmail.com>
2021-04-10 09:17:17 -0700

    Update models.js
    fixed typo

--- models.js
+++ models.js
@@ -32,7 +32,7 @@ userSchema.statics.hashPassword = password => {
 };
 
 userSchema.methods.validatePassword = function(password) {
-  return bcrypt.compareSync(password, this.password);
+  return bcrypt.compareSync(password, this.Password);
 };
 
 let genreSchema = mongoose.Schema({
