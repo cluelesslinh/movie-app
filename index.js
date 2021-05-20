b21f49d86f4f197936a8cfafd1f69793e1605c0c
Linh Pham <77943437+cluelesslinh@users.noreply.github.com>
2021-05-19 20:58:18 -0700

    Update index.js
    update cors

--- index.js
+++ index.js
@@ -33,7 +33,18 @@ app.use(express.static("public"));
 
 let auth = require("./auth")(app);
 
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
